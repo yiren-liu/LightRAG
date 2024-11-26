2bd793dd23a877f655f77ef07d694db0d34efcd2
Yiren Liu <yirenl2@illinois.edu>
2024-11-26 14:25:42 -0600

    testing new example paper concept mining

--- examples/lightrag_openai_compatible_demo.py
+++ examples/lightrag_openai_compatible_demo.py
@@ -5,7 +5,12 @@ from lightrag.llm import openai_complete_if_cache, openai_embedding
 from lightrag.utils import EmbeddingFunc
 import numpy as np
 
-WORKING_DIR = "./dickens"
+import dotenv
+
+dotenv.load_dotenv()
+
+
+WORKING_DIR = "./temp"
 
 if not os.path.exists(WORKING_DIR):
     os.mkdir(WORKING_DIR)
@@ -15,12 +20,12 @@ async def llm_model_func(
     prompt, system_prompt=None, history_messages=[], **kwargs
 ) -> str:
     return await openai_complete_if_cache(
-        "solar-mini",
+        "gpt-4o",
         prompt,
         system_prompt=system_prompt,
         history_messages=history_messages,
-        api_key=os.getenv("UPSTAGE_API_KEY"),
-        base_url="https://api.upstage.ai/v1/solar",
+        api_key=os.getenv("OPENAI_API_KEY"),
+        base_url=os.getenv("OPENAI_API_BASE"),
         **kwargs,
     )
 
@@ -28,11 +33,10 @@ async def llm_model_func(
 async def embedding_func(texts: list[str]) -> np.ndarray:
     return await openai_embedding(
         texts,
-        model="solar-embedding-1-large-query",
-        api_key=os.getenv("UPSTAGE_API_KEY"),
-        base_url="https://api.upstage.ai/v1/solar",
+        model="text-embedding-3-small",
+        api_key=os.getenv("OPENAI_API_KEY"),
+        base_url=os.getenv("OPENAI_API_BASE"),
     )
-
 
 async def get_embedding_dim():
     test_text = ["This is a test sentence."]
@@ -68,7 +72,7 @@ async def main():
             ),
         )
 
-        with open("./book.txt", "r", encoding="utf-8") as f:
+        with open("./data/UX.txt", "r", encoding="utf-8") as f:
             await rag.ainsert(f.read())
 
         # Perform naive search
